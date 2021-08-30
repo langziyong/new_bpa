afec6158f4ceb6170efed3b1eb07ff2c7b90605d
Langziyong <1837854209@qq.com>
2021-08-31 00:48:25 +0800

    0831

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import flask_mysql
 
 app = Flask(__name__)
 
@@ -10,13 +11,20 @@ def index():  # put application's code here
 
 @app.route('/search', methods = ['POST'])
 def seach():
-    pass
-
+    xh = request.form.get('xh')
+    data = flask_mysql.getUser(xh)
+    return str(data)
 
 @app.route('/registered', methods = ['POST', 'GET'])
 def registerde():
     if request.method == 'GET':
         return render_template('registered.html')
+    elif request.method == 'POST':
+        req = request.form
+        if not flask_mysql.addUser(req):
+            return '提交失败'
+        else:
+            return '提交成功'
 
 
 if __name__ == '__main__':
--- flask_mysql.py
+++ flask_mysql.py
@@ -9,5 +9,39 @@ def addUser(newuserdata):
         database = 'bpa_user_data',
         port = 3306)
     cursor = db.cursor()
-    cursor.execute('SELECT * from user_data')
-    data = cursor.fetchone()
+    cursor.execute('select count(1) from user_data')
+    id_number = cursor.fetchall()[0][0] + 1
+    sql = '''INSERT INTO user_data(id, xh, xm, mm, xy, sjhm, dz1, dz2, xxdz, tw1, tw2, email) 
+             VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');
+             ''' % \
+          (id_number, newuserdata['xh'], newuserdata['xm'], newuserdata['mm'],
+           newuserdata['xy'], newuserdata['sjhm'], newuserdata['dz1'], newuserdata['dz2'],
+           newuserdata['xxdz'], newuserdata['tw1'], newuserdata['tw2'], newuserdata['email'])
+
+    try:
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+        return True
+    except:
+        db.rollback()
+        db.close()
+        return False
+
+
+def getUser(xh):
+    db = pymysql.connect(
+        host = '114.55.140.138',
+        user = 'root',
+        password = "lzy0812..",
+        database = 'bpa_user_data',
+        port = 3306)
+    cursor = db.cursor()
+    sql = '''
+    SELECT * From user_data WHERE xh = '%s';''' % xh
+    cursor.execute(sql)
+    print(sql)
+    data = cursor.fetchall()
+    db.close()
+    print(data)
+    return data
